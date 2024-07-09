cf94568ede465f31ad93620c751d6235188140b5
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-07-09 21:21:11 +0530

    changing config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-};
+    // output: 'export',
+    // typescript: {
+    //     ignoreBuildErrors: true,
+    // },
+}
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
